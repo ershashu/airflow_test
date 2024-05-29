3c30b97cd24c67741f9e56e065a0e74cf4d0972e
shashank-mahajan <shashank.mahajan@hpe.com>
2024-05-29 21:40:47 +0530

    test

--- dags/email-local.py
+++ dags/email-local.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from airflow import DAG
+from airflow.models.param import Param
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 from smtplib import SMTP
