b53a1c46cf99255fda7ede642adb329ecfc07b4a
Shashank Mahajan <shashank.mahajan@hpe.com>
2026-02-02 16:37:56 +0530

    test

--- dags/badssl.py
+++ dags/badssl.py
@@ -21,7 +21,7 @@ with DAG(
     start_date=datetime(2024, 1, 1),
     schedule=None, 
     catchup=False,
-    access_control={"All": {"can_read", "can_edit", "can_delete"}},
+    access_control={"All": {"can_read", "can_edit"}},
 ) as dag:
 
     ca_cert_test = PythonOperator(
