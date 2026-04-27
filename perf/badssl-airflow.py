b77a1bd658f5ad578af9e66da0fee10fca02a675
Shashank Mahajan <shashank.mahajan@hpe.com>
2026-04-27 14:36:46 +0530

    CA Cert Test python

---
+++ perf/badssl-airflow.py
@@ -0,0 +1,30 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+import requests
+import os
+
+def test_ca_cert():
+    print("Testing HTTPS with custom CA")
+
+    ca_path = os.getenv("EZUA_DOMAIN_CA_CERT_PATH")
+    print("EZUA_DOMAIN_CA_CERT_PATH:", ca_path)
+
+    response = requests.get(
+        "https://untrusted-root.badssl.com/",
+        timeout=30,
+    )
+    print("Status code:", response.status_code)
+
+with DAG(
+    dag_id="ca_cert_test_python_operator",
+    start_date=datetime(2024, 1, 1),
+    schedule=None, 
+    catchup=False,
+    access_control={"All": {"can_read", "can_edit", "can_delete"}},
+) as dag:
+
+    ca_cert_test = PythonOperator(
+        task_id="ca_cert_test",
+        python_callable=test_ca_cert,
+    )
