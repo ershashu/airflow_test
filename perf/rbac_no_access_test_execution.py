f2a6252b7216fd5ef64251974f9522c401c45e2c
Shashank Mahajan <shashank.mahajan@hpe.com>
2026-04-27 10:49:21 +0530

    No access

---
+++ perf/rbac_no_access_test_execution.py
@@ -0,0 +1,50 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.models.param import Param
+from airflow.decorators import task
+from airflow.operators.python import get_current_context
+from datetime import datetime, timedelta
+import time
+
+
+args = {
+    'owner': 'airflow',
+    'start_date': datetime.now() - timedelta(days=1),
+}
+
+with DAG(
+    dag_id='rbac_delete_test',
+    default_args=args,
+    tags=['example', 'performance_benchmarking', 'parallel_tasks'],
+    params={
+        'count': Param(30, type="integer", minimum=0),
+        'sleep': Param(120, type="integer", minimum=0),
+    },
+    render_template_as_native_obj=True,
+    max_active_runs=2048,
+    max_active_tasks=2048,
+    access_control={
+        'role_ershashu': {
+            
+        } 
+    }
+) as dag:
+    @task
+    def get_count():
+        context = get_current_context()
+        return list(range(context['params']['count']))
+
+    run_this = BashOperator(
+        task_id='run_after_loop',
+        bash_command='echo test',
+        dag=dag,
+    )
+
+    @task
+    def runme(i):
+        print(i)
+        context = get_current_context()
+        time.sleep(context['params']['sleep'])
+
+
+    runme.expand(i=get_count()) >> run_this
